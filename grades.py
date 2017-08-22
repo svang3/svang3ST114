807ca5ff8cc10c4e4819700358cdf914baa3e3f9
svang3 <svang3@ncsu.edu>
2017-08-22 15:16:07 -0400

    Initial Commit

---
+++ grades.py
@@ -0,0 +1,53 @@
+# Student
+# student = 
+
+# Initialize homework scores list
+
+homework = []
+
+# Homework 1
+# homework.append()
+
+# Homework 2
+# homework.append()
+
+# Homework 3
+# homework.append()
+
+# Homework 4
+# homework.append()
+
+# Homework 5
+# homework.append()
+
+# Homework 6
+# homework.append()
+
+# Homework 7
+# homework.append()
+
+# Homework 8
+# homework.append()
+
+# Homework 9
+# homework.append()
+
+# Homework 10
+# homework.append()
+
+# Homework 11
+# homework.append()
+
+# Homework 12
+# homework.append()
+
+print(homework)
+
+# Midterm score
+# midterm = 
+
+# Final project score
+# final_project = 
+
+# Absences
+# number_absent =
